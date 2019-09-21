f6e5f87baadcd5667ee2bb7e58d1a4884e73942f
AndreyGnuchikh <44124200+andreygnuchikh@users.noreply.github.com>
2019-09-21 10:05:25 +0300

    Initial commit
    Signed-off-by: andrey <andreygnuchikh@yahoo.com>

---
+++ 2.2.3 String in Int and Select.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+link = "http://suninjuly.github.io/selects1.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+# Ваш код, который заполняет обязательные поля
+...
+
+# Отправляем заполненную форму
+num1 = browser.find_element_by_css_selector("#num1")
+num2 = browser.find_element_by_css_selector("#num2")
+x = int(num1.text)
+y = int(num2.text)
+
+print(x)
+print(y)
+i: int = (x+y)
+print(i)
+# Проверяем, что смогли зарегистрироваться
+# ждем загрузки страницы
+time.sleep(1)
+i = i.__str__()
+select = Select(browser.find_element_by_id("dropdown"))
+select.select_by_visible_text(i)
+
+browser.find_element_by_css_selector(".btn-default").click()
