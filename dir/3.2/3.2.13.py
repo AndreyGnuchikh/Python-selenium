a9a25158049a02ebd7cc4f3ef400c2651c21fa9f
andrey <andreygnuchikh@yahoo.com>
2019-10-22 23:14:16 +0300

    add 3.2.13 and 1.16.11

--- dir/3.2/3.2.13.py
+++ dir/3.2/3.2.13.py
@@ -3,9 +3,6 @@ from datetime import time
 
 from selenium import webdriver
 
-link = "http://suninjuly.github.io/registration1.html"
-link2 = "http://suninjuly.github.io/registration2.html"
-
 
 class TestAbs(unittest.TestCase):
 
@@ -14,8 +11,40 @@ class TestAbs(unittest.TestCase):
         browser = webdriver.Chrome()
         browser.get(link)
 
+        input1 = browser.find_element_by_tag_name('input')
+        input1.send_keys("Ivan")
+        input1 = browser.find_element_by_css_selector('.first_block .form-control.second')
+        input1.send_keys("Ivan")
+        input1 = browser.find_element_by_css_selector('.first_block .form-control.third')
+        input1.send_keys("Ivan")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
+
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
         # Ваш код, который заполняет обязательные поля
-        ...
+        input1 = browser.find_element_by_tag_name("input")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class input")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_class_name("third")
+        input3.send_keys("Smolensk@gmail.com")
+        input4 = browser.find_element_by_css_selector("div.second_block div.form-group.first_class input")
+        input4.send_keys("+79269499824")
+        input4 = browser.find_element_by_tag_name("div.second_block div.form-group.second_class input")
+        input4.send_keys("Russia")
 
         # Отправляем заполненную форму
         button = browser.find_element_by_css_selector("button.btn")
@@ -29,25 +58,7 @@ class TestAbs(unittest.TestCase):
         welcome_text_elt = browser.find_element_by_tag_name("h1")
         # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elt.text
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
         self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
-
-    def test_abs2(self):
-        browser = webdriver.Chrome()
-        browser.get(link2)
-
-        input1 = browser.find_element_by_tag_name("input")
-        input1.send_keys("Ivan")
-        input2 = browser.find_element_by_css_selector(
-            "body > div > form > div.first_block > div.form-group.third_class > input")
-        input2.send_keys("123")
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
-        welcome_text_elt2 = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
-        welcome_text2 = welcome_text_elt2.text
-        self.assertEqual(welcome_text2, "Congratulation2s! You have successfully registered!")
 
 
 if __name__ == "__main__":
